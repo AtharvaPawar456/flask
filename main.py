5acc827c86d4efd3e7a42d0482ae4eebc7ff1405
Atharva Prashant Pawar <67649944+AtharvaPawar456@users.noreply.github.com>
2023-02-11 16:02:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from flask import Flask, request
-import json
-import time
 import pymongo
 
 app = Flask(__name__)
@@ -14,13 +12,13 @@ def handle_request():
     text3 = str(request.args.get('input3'))
     character_count = len(text1) + len(text2) + len(text3)
 
-    data_set = {'input1': text1, 'input2': text2, 'input3': text3, 'timestamp': time.time(), 'character_count': character_count}
-    json_dump = json.dumps(data_set)
-
     client = pymongo.MongoClient(connect_Db_link)
     db = client.get_database('Students_db')
     records = db.students_record
-    dictionary = {'name': text1, 'roll_no': text2, 'mks': text3}
+    dictionary = {'name': text1, 'roll_no': text2, 'mks': text3, 'CharCount': character_count}
     records.insert_one(dictionary)
 
-    return json_dump
+    return "Data uploaded successfully."
+
+if __name__ == '__main__':
+    app.run(debug=True)
